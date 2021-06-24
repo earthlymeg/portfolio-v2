d68bf66e91766f1b486b56f0d147ed1138f006a1
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-06-23 22:21:35 -0600

    Adding key frames

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,8 @@ function LandingPage() {
   return (
     <div className="landing-page">
       <div className="landing-logo">
-        <h1>Meg</h1> <br></br>
-        <h2>Shulmister</h2> <br></br>
+        <h1>MEG</h1> <br></br>
+        <h2>SHULMISTER</h2> <br></br>
         <h3>Full Stack Developer</h3>
       </div>
     </div>
