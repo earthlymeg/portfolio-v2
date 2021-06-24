1f12b62353dd9a3f2c8c92c9707ff9b8fa6bb8cd
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-06-23 18:14:09 -0600

    First commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import "../styles/landingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="landing-logo">
+        <h1>Meg</h1> <br></br>
+        <h2>Shulmister</h2> <br></br>
+        <h3>Full Stack Developer</h3>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
