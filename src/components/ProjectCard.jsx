bbc5f5c7f76e45392fbe9a3d6d74bf3e1ab89b0e
earthlymeg <meganshulmister@gmail.com>
2021-08-04 08:52:39 -0600

    card-styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,18 @@
 import "../styles/projectCard.css";
 
 function ProjectCard() {
-  return <div className="card">
-      {/* <img src=""></img> */}
-      <h2>Title</h2>
-      <p>Description</p>
-
-  </div>;
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+        alt=""
+      ></img>
+      <div>
+        <h2>Title</h2>
+        <p>Description</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
