83673cc55f4e0e54e926821cddf77a39a11bbdc8
earthlymeg <meganshulmister@gmail.com>
2021-08-04 08:31:12 -0600

    staging landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,29 +1,10 @@
-// import "../styles/landingPage.css";
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import Header from './Header';
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: '100vh',
-    backgroundColor: '#c8b5a6',
 
-
-  }
-}));
 
 function LandingPage() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-        <CssBaseline/>
-        
-      {/* <div className="landing-logo">
-        
-        <h1>MEG</h1> <br></br>
-        <h2>SHULMISTER</h2> <br></br>
-        <h3>Full Stack Developer</h3>
-      </div> */}
-        <Header />
+       Will be landing page
     </div>
   );
 }
