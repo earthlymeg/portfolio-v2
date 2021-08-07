b10d505c4736aefe2e1cdb1c39ca926a68c989e9
earthlymeg <meganshulmister@gmail.com>
2021-08-07 11:09:18 -0600

    Styling responsive cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,15 @@
 import "../styles/projectCard.css";
 
 function ProjectCard() {
-  return <div className="card">
+  return (
+    <div className="card">
       <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
-      <h3>Title</h3>
-      <p>Description</p>
-
-  </div>;
+      <div className="name-description">
+        <h3>Title</h3>
+        <p>Description</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
