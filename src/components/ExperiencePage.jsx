147c37810de8c1a47ee6290b7bcc5a7dde937ccf
earthlymeg <meganshulmister@gmail.com>
2021-08-06 18:01:48 -0600

    screwed up styling

--- src/components/ExperiencePage.jsx
+++ src/components/ExperiencePage.jsx
@@ -4,10 +4,12 @@ function ExperiencePage() {
 
     return (
         <div className="experience-page">
-            <h1>Experience Page</h1>
+            <section>
+            <h1>Experience</h1>
             <ProjectCard/>
             {/* <ProjectCard/>
             <ProjectCard/> */}
+            </section>
         </div>
     )
 }
