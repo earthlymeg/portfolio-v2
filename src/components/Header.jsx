7f524fc6ff389b28f212f8577e81de73e052023e
earthlymeg <meganshulmister@gmail.com>
2021-08-07 10:34:55 -0600

    Adding experience page styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,28 +4,30 @@ function Header() {
   return (
     <header className="header">
       <section>
-      <div className="intro">
-      <h3 className="name">Meg Shulmister</h3>
-        <h1>
-          {" "}
-          Hello,<br></br>
-          I'm Meg Shulmister
-        </h1>
-        <p>
-          I'm a full-stack software engineer based in Rollinsville, Colorado.
-          Thanks for stopping by.
-        </p>
+        <div className="intro">
+          <h3 className="name">Meg Shulmister</h3>
+          <h1>
+            {" "}
+            Hello,<br></br>
+            I'm Meg Shulmister
+          </h1>
+          <p>
+            I'm a full-stack software engineer based in Rollinsville, Colorado.
+            Thanks for stopping by.
+          </p>
 
-        <button>
-          <a href="mailto:meganshulmister@gmail.com">Email Me</a>
-        </button>
-        <button>
-          <a href="https://www.linkedin.com/in/meganshulmister019/">LinkedIn</a>
-        </button>
-        <button>
-          <a href="https://github.com/earthlymeg">GitHub</a>
-        </button>
-</div>
+          <button>
+            <a href="mailto:meganshulmister@gmail.com">Email Me</a>
+          </button>
+          <button>
+            <a href="https://www.linkedin.com/in/meganshulmister019/">
+              LinkedIn
+            </a>
+          </button>
+          <button>
+            <a href="https://github.com/earthlymeg">GitHub</a>
+          </button>
+        </div>
         <div className="headshot">
           <img src={headshot} alt="Meg Shulmister portfoli0"></img>
         </div>
