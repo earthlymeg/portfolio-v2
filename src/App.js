24ca41e179757da8305dcdc7a8977e92c7ae9c76
earthlymeg <meganshulmister@gmail.com>
2021-08-10 21:36:23 -0600

    styling footer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className='App'>
     <Header />
     <ExperiencePage/>
-    {/* <Footer/> */}
+    <Footer/>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,25 @@
+
 import '../styles/footer.css'
+import {FiArrowUpRight} from 'react-icons/fi';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 function Footer() {
+
+
   return (
     <footer>
       <div className="footer">
         <h3>Meg Shulmister</h3>
         <div className="footer-btns">
-        <button>
-          <a href="mailto:meganshulmister@gmail.com">Email Me</a>
+        <button >
+          <a href="mailto:meganshulmister@gmail.com">Email<FiArrowUpRight/></a>
         </button>
-        <button>
-          <a href="https://www.linkedin.com/in/meganshulmister019/">LinkedIn</a>
-        </button>
-        <button>
-          <a href="https://github.com/earthlymeg">GitHub</a>
+        <button >
+          <a href="https://www.linkedin.com/in/meganshulmister019/">LinkedIn<FiArrowUpRight/></a>
+        </button >
+        <button >
+          <a href="https://github.com/earthlymeg">GitHub<FiArrowUpRight/></a>
         </button>
         </div>
       </div>
